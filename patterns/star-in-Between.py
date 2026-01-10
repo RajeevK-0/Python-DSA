78f7368c4b1be98aa4fba25bf3d58a2870028d75
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-01-10 22:47:48 +0530

    new pattern

---
+++ patterns/star-in-Between.py
@@ -0,0 +1,19 @@
+N = int(input())
+i = 0
+while i < 2*N-1:
+    if i == 0 or i == 2*N-2:
+        stars = (2*N-1) *'*'
+        print(stars)
+    else:
+            if i<N:
+                stars = (N - i)*'*'
+                space = (2*i-1)*' '
+                s = stars+space+stars
+                print(s)
+            else:
+                j = i-N+2
+                stars = j*'*'
+                space = ((2*N -1)-2*j)*' '
+                s = stars+space+stars
+                print(s)
+    i+=1
