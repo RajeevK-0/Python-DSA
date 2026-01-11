799d2491aa4b884118157467106260f143a52fde
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-01-12 00:06:30 +0530

    correction

--- patterns/hollowDiamondPattern.py
+++ patterns/hollowDiamondPattern.py
@@ -3,11 +3,9 @@ mid = n // 2
 
 for i in range(n):
     for j in range(n):
-        if (
-            i == 0 or i == n-1 or j == 0 or j == n-1 or
-            abs(i - mid) + abs(j - mid) == mid
-        ):
-            print("*", end="    ")
-        else:
+        d = abs(i - mid) + abs(j - mid) 
+        if d<mid:
             print(" ", end="    ")
+        else:
+            print("*", end="    ")
     print()
