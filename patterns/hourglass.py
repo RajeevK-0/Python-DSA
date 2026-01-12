933e32b626a6dbd9c259400aae125aac9ed4254a
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-01-12 23:10:41 +0530

    Update hourglass.py

--- patterns/hourglass.py
+++ patterns/hourglass.py
@@ -1,46 +1,68 @@
 N = int(input())
-i = 1
-while i <= N:
-    space = i-1
-    j = 1
-    while j<=space:
+for i in range(-N,(N+1)):
+    row = abs(i)
+    space = N-row
+    while space:
         print(' ',end=' ')
-        j+=1
-    t = N-i+1
-    while t != 0:
-        print(t, end=' ')
-        t-=1
-    print('0',end = ' ')
-    t = N-i+1
-    k = 1
-    while k<=t:
-        print(k,end = ' ')
-        k+=1
-    i+=1
+        space-=1
+    for j in range(row,-1,-1):
+        print(j,end=' ')
+    if row!=0:    
+        for j in range(1,row+1):
+            print(j,end=' ')
     print()
-i = 1
-while i<=N:
-    if i == N:
-        print('  0',end=' ')
-    else:    
-        print(' ',end = ' ')
-    i+=1
-print()
-i = 1
-while i <= N:
-    space = N-i
-    k = 1
-    while k<=space:
-        print(' ',end = ' ')
-        k+=1
-    num = N - space
-    while num !=0:
-        print(num,end=' ')
-        num -= 1
-    print('0',end=' ')
-    k = 1
-    while k<=i:
-        print(k, end=' ')
-        k+=1 
-    i+=1
-    print()
+
+
+
+
+
+
+
+
+
+
+# i = 1
+# while i <= N:
+#     space = i-1
+#     j = 1
+#     while j<=space:
+#         print(' ',end=' ')
+#         j+=1
+#     t = N-i+1
+#     while t != 0:
+#         print(t, end=' ')
+#         t-=1
+#     print('0',end = ' ')
+#     t = N-i+1
+#     k = 1
+#     while k<=t:
+#         print(k,end = ' ')
+#         k+=1
+#     i+=1
+#     print()
+# i = 1
+# while i<=N:
+#     if i == N:
+#         print('  0',end=' ')
+#     else:    
+#         print(' ',end = ' ')
+#     i+=1
+# print()
+# i = 1
+# while i <= N:
+#     space = N-i
+#     k = 1
+#     while k<=space:
+#         print(' ',end = ' ')
+#         k+=1
+#     num = N - space
+#     while num !=0:
+#         print(num,end=' ')
+#         num -= 1
+#     print('0',end=' ')
+#     k = 1
+#     while k<=i:
+#         print(k, end=' ')
+#         k+=1 
+#     i+=1
+#     print()
