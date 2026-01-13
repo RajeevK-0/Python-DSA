54ec2b6672d7a1cdf5f2d503648bc58b79cbfd71
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-01-13 11:04:48 +0530

    Update star-in-Between.py

--- patterns/star-in-Between.py
+++ patterns/star-in-Between.py
@@ -1,19 +1,36 @@
 N = int(input())
-i = 0
-while i < 2*N-1:
-    if i == 0 or i == 2*N-2:
-        stars = (2*N-1) *'*'
-        print(stars)
-    else:
-            if i<N:
-                stars = (N - i)*'*'
-                space = (2*i-1)*' '
-                s = stars+space+stars
-                print(s)
-            else:
-                j = i-N+2
-                stars = j*'*'
-                space = ((2*N -1)-2*j)*' '
-                s = stars+space+stars
-                print(s)
-    i+=1
+mid = N-1
+for i in range(-mid , mid+1):
+    row = abs(i)
+    for j in range(-mid , mid+1):
+        col = abs(j)
+        s = abs(mid-row)+abs(mid-col)
+        if s > mid:
+            print(" ",end='')
+        else:
+            print('*',end='')
+    print()
+
+
+
+
+
+
+# i = 0
+# while i < 2*N-1:
+#     if i == 0 or i == 2*N-2:
+#         stars = (2*N-1) *'*'
+#         print(stars)
+#     else:
+#             if i<N:
+#                 stars = (N - i)*'*'
+#                 space = (2*i-1)*' '
+#                 s = stars+space+stars
+#                 print(s)
+#             else:
+#                 j = i-N+2
+#                 stars = j*'*'
+#                 space = ((2*N -1)-2*j)*' '
+#                 s = stars+space+stars
+#                 print(s)
+#     i+=1
