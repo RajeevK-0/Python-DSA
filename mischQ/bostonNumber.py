e8c84f83a16b981095f8c2b86d77e3bd051864c0
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-01-13 22:49:35 +0530

    Create bostonNumber.py

---
+++ mischQ/bostonNumber.py
@@ -0,0 +1,22 @@
+N= int(input())
+def sumOfDigits(n):
+    sum = 0 
+    while n:
+        sum += n%10
+        n //= 10
+    return sum
+def primeFactorsSum(n):
+    a = 2
+    sum = 0 
+    
+    while n>1:
+        while n%a==0:
+            sum+= sumOfDigits(a)
+            n //= a
+        a+=1
+    return sum
+
+def isBoston(N):
+    return primeFactorsSum(N) == sumOfDigits(N)
+
+print(isBoston(N))
