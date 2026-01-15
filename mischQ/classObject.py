5bf47d2fc0dc75c95b15a2864fb7ebb5fce9d27c
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-01-15 15:10:46 +0530

    Update classObject.py

--- mischQ/classObject.py
+++ mischQ/classObject.py
@@ -1,4 +1,21 @@
+from abc import ABC,abstractmethod
 class person:
-    def __init__(self,name,age):
+    #dunder
+    def __init__(self,name = 'xyz',age = 22):
         self.name = name
         self.age = age
+    def greet(self,name = 'xyz'):
+        print(f"hello {name}, how was your day ")
+    def prin(self):
+        print(self.name*4)
+'''pr inherit person class
+    OOPs-inheritance'''
+class pr(person):
+    pass #placholder for future code
+prs = pr()
+prs.greet()
+
+
+# p = person()
+# p.greet()
+# p.prin()
