2706aa07063cf970d5a72232ea455f6e2d0f5577
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-01-15 15:45:03 +0530

    Update classObject.py

--- mischQ/classObject.py
+++ mischQ/classObject.py
@@ -14,7 +14,7 @@ class pr(person):
     pass #placholder for future code
 prs = pr()
 prs.greet()
-
+#Encapsulation.....
 class BankAccount:
     def __init__(self, owner, balance):
         self.owner = owner       # Public
@@ -35,6 +35,21 @@ account.deposit(500)
 # print(account.__balance) # This would raise an AttributeError
 print(account.get_balance()) # Output: 1500
 print(account.owner)
-# p = person()
-# p.greet()
-# p.prin()
+###Inheritance
+# Parent Class
+class Animal:
+    def speak(self):
+        print("Animal makes a sound")
+
+# Child Class inherits from Animal
+class Cat(Animal):
+    def speak(self):
+        print("Meow")
+
+# Child Class inherits from Animal
+class Dog(Animal):
+    def speak(self):
+        print("Woof")
+my_cat = Cat()
+my_cat.speak() # Output: Meow (overrides parent method)
+
