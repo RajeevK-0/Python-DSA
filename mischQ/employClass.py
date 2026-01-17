70236eb4bd51d704b426e00dde7531063f62ea48
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-01-17 21:59:52 +0530

    Create employClass.py

---
+++ mischQ/employClass.py
@@ -0,0 +1,20 @@
+#employee -- empid , name, salary
+class employee:
+    def __init__(self,empid = None , name = None, salary = None):
+        self.empid = empid  
+        self.name = name
+        self.salary = salary
+    def setEmpid(self,empid):
+        self.empid = empid
+    def setName(self,name):
+        self.name = name
+    def setSalary(self,salary):
+        self.salary = salary
+    def getEmpid(self):
+        return self.empid
+    def getName(self):
+        return self.name
+    def getSalary(self):
+        return self.salary
+
+e1 = employee(1234,'rajeev',50000)
