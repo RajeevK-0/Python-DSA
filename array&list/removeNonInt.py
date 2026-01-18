7c8d74ab812b0a203d012b5cd9f75571ba2b0379
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-01-18 15:12:00 +0530

    corrected

--- array&list/removeNonInt.py
+++ array&list/removeNonInt.py
@@ -6,8 +6,10 @@ print(l)
 print(type(l[0]))
 if type(l[0]) == int:
     print('yes its int')
-for i in l:
-    if type(i) != int:
-        l.remove(i)
+# this code will give problem cuz shift will be occuring as we remove i, so use either list comprehension or start from last i
+# for i in l:
+#     if type(i) != int:
+#         l.remove(i)
+l = [i for i in l if type(i)== int]
 print('after')
 print(l)
