120baae29abd1333f099bc6176c48ea82343f8e7
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-01-18 14:37:57 +0530

    Update sortingArray.py

--- array&list/sortingArray.py
+++ array&list/sortingArray.py
@@ -3,11 +3,12 @@ a = array('i', [2,1,3,2,4,3,6,33,7,6])
 #sort this array
 l = len(a)
 for i in range(l):
-    for j in range(l-i+1,l):
+    for j in range(i+1,l):
         if a[i]>a[j]:
-            t = a[i]
-            a[i] = a[j]
-            a[j] = t
+            # t = a[i]
+            # a[i] = a[j]
+            # a[j] = t
+            a[i],a[j] = a[j],a[i]
 b = sorted(a)
 print(sorted(a))
 print(b)
