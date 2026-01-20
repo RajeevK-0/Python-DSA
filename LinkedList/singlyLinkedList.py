446ed3f4df8ea345c75c680d626bfc57aca3a3cd
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-01-20 20:54:10 +0530

    Update singlyLinkedList.py

--- LinkedList/singlyLinkedList.py
+++ LinkedList/singlyLinkedList.py
@@ -84,24 +84,24 @@ class SLL:
         #     if self.next() == element.item():
         #         return self.next()
         # self.next() ==  temp
-    # def __iter__(self):
-    #     start = self.start
-    #     while start is not None:
-    #         yield start.item
-    #         start = start.next
     def __iter__(self):
-        return sllItrator(self.start)
-class sllItrator:
-    def __init__(self,start):
-        self.current = start
-    def __iter__(self):
-        return self
-    def __next__(self):
-        if not self.current:
-            raise StopIteration
-        data = self.current.item
-        self.current = self.current.next
-        return data
+        start = self.start
+        while start is not None:
+            yield start.item
+            start = start.next
+#     def __iter__(self):
+#         return sllItrator(self.start)
+# class sllItrator:
+#     def __init__(self,start):
+#         self.current = start
+#     def __iter__(self):
+#         return self
+#     def __next__(self):
+#         if not self.current:
+#             raise StopIteration
+#         data = self.current.item
+#         self.current = self.current.next
+#         return data
 myList = SLL()
 myList.insertAtStart(10)
 myList.insertAtStart(20)
