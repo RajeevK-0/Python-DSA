8f4e12079c4d4f904720bb23fe9ebc2f910e3658
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-01-23 22:13:06 +0530

    Update stack-exList.py

--- stack/stack-exList.py
+++ stack/stack-exList.py
@@ -1,5 +1,29 @@
 class stack(list):
-    def __init__(self):
-        pass
+        
     def is_empty(self):
-        pass
+        return len(self) == 0
+    def push(self,item):
+        self.append(item)
+    def pop(self):
+        if not self.is_empty():
+            return super().pop()#overriding could happen
+        else:
+            raise IndexError('list is empty')
+    def peek(self):
+        if not self.is_empty():    
+            return self[-1]
+        else:
+            raise IndexError('list is empty')
+    def size(self):
+        return len(self)
+    def insert(self,index,data):
+        raise AttributeError("No attribute 'insert' in stack")
+
+s = stack()
+s.push(10)
+s.push(20)
+s.push(30)
+print(s.peek())
+s.pop()
+print(s.peek())
+print(s.size())
