b33c7b0d88c91e6da5cf056c182dc355b74604b9
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-01-27 21:57:28 +0530

    Create productxceptSelf.py

---
+++ array&list/productxceptSelf.py
@@ -0,0 +1,12 @@
+class Solution:
+    def productExceptSelf(self, nums: list[int]) -> list[int]:
+        ans = [1]*len(nums)
+        l = 1
+        for i in range(len(nums)):
+            ans[i] = l
+            l *= nums[i]
+        r = 1
+        for j in range(len(nums)-1,-1,-1):
+            ans[j] *= r
+            r *= nums[j]
+        return ans
