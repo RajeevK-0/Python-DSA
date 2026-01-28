ccaf812ba2f749cadf7c838ab937eaeb17ba9ab6
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-01-28 23:35:36 +0530

    Update validPalindrome.py

--- array&list/validPalindrome.py
+++ array&list/validPalindrome.py
@@ -1,18 +1,18 @@
-# class Solution:
-#     def isPalindrome(self, s: str) -> bool:
-#         i = 0 
-#         j = len(s)-1
-#         while i<j:
-#             if not s[i].isalnum():
-#                 i+=1
-#             elif not s[j].isalnum():
-#                 j-=1
-#             else:
-#                 if s[i].lower()!=s[j].lower():
-#                     return False
-#                 i+=1
-#                 j-=1
-#         return True
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i = 0 
+        j = len(s)-1
+        while i<j:
+            if not s[i].isalnum():
+                i+=1
+            elif not s[j].isalnum():
+                j-=1
+            else:
+                if s[i].lower()!=s[j].lower():
+                    return False
+                i+=1
+                j-=1
+        return True
 # class Solution:
 #     def isPalindrome(self, s: str) -> bool:
 #         i = 0 
@@ -27,15 +27,15 @@
 #             i+=1
 #             j-=1
 #         return True
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        t = "".join(c for c in s if c.isalnum())
-        t = t.lower()
-        i = 0
-        j = len(t)-1
-        while i<=j:
-            if t[i] != t[j]:
-                return False 
-            i+=1
-            j-=1
-        return True
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         t = "".join(c for c in s if c.isalnum())
+#         t = t.lower()
+#         i = 0
+#         j = len(t)-1
+#         while i<=j:
+#             if t[i] != t[j]:
+#                 return False 
+#             i+=1
+#             j-=1
+#         return True
