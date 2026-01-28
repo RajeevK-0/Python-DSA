55a2580aad38404c432fb720e6d58f680921634d
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-01-28 23:34:55 +0530

    Update validPalindrome.py

--- array&list/validPalindrome.py
+++ array&list/validPalindrome.py
@@ -13,17 +13,29 @@
 #                 i+=1
 #                 j-=1
 #         return True
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         i = 0 
+#         j = len(s)-1
+#         while i<j:
+#             while i<j and not s[i].isalnum():
+#                 i+=1
+#             while i<j and not s[j].isalnum():
+#                 j-=1
+#             if s[i].lower()!=s[j].lower():
+#                 return False
+#             i+=1
+#             j-=1
+#         return True
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        i = 0 
-        j = len(s)-1
-        while i<j:
-            while i<j and not s[i].isalnum():
-                i+=1
-            while i<j and not s[j].isalnum():
-                j-=1
-            if s[i].lower()!=s[j].lower():
-                return False
+        t = "".join(c for c in s if c.isalnum())
+        t = t.lower()
+        i = 0
+        j = len(t)-1
+        while i<=j:
+            if t[i] != t[j]:
+                return False 
             i+=1
             j-=1
         return True
