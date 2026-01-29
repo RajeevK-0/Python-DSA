e67d9f149db789a5a4a74a3e800d1f3fc435bf7d
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-01-29 23:15:53 +0530

    Update longestSubstring.py

--- sliding window/longestSubstring.py
+++ sliding window/longestSubstring.py
@@ -9,3 +9,15 @@ class Solution:
             d[s[i]] = i
             c = max(c,i-j+1)
         return c
+    # c = 0
+        # for i in range(len(s)):
+        #     j = i
+        #     if s[i] in d:
+        #         while j<=i:
+        #             d[s[j]]-=1
+        #             j+=1
+        #     else:
+        #         d[s[i]] = 1
+        #     c = max(c,len(d.keys()))
+        # return c 
+                
