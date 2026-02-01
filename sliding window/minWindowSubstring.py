47c58731c3ec1a6c2f21884571b5ae992e54ebcb
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-02-01 22:22:19 +0530

    Update minWindowSubstring.py

--- sliding window/minWindowSubstring.py
+++ sliding window/minWindowSubstring.py
@@ -30,5 +30,48 @@ class Solution:
 
 
 
-
-        
+ # if len(s)<len(t):
+        #     return ""
+        # if len(s) == len(t):
+        #     if sorted(s)== sorted(t):
+        #         return s
+        #     return ""
+        # start = 0
+        # dt = {}
+        # for i in t:
+        #     dt[i] = dt.get(i,0)+1
+        # scount = 0
+        # ds={}
+        # for end in range(len(s)):
+        #     if s[end] in dt and scount == 0:
+        #         start = end
+        #     if s[end] in dt:
+        #         scount+=1
+        #     if scount >= len(dt.keys()):
+        #         res = start,end
+        #         start+=1
+        #         while s[start] not in dt:
+        #             start +=1
+        #         scount-=1
+        #         rs = s[res[0]:res[1]+1]
+        # return rs
+        # start = 0
+        # if len(s)<len(t):
+        #     return res
+        # d = {}
+        # ds = {}
+        # for i in t:
+        #     d[i] = d.get(i ,0)+1
+        # for end in range(len(s)):
+        #     if s[end] in d and s[start] not in d:
+        #         start = end
+        #     ds[s[end]] = ds.get(s[end],0)+1
+        #     c = 0
+        #     for i in d.keys():
+        #         if i in ds and ds[i]>=d[i]:
+        #             c+=1
+        #     if len(d.keys())==c:
+        #         res = s[start:end+1]
+        #         if len(res)>end-start+1:
+        #             res = s[start:end+1]
+        # return res                
