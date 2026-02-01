fdb835e3fe1c25aef86a25f41035249e4e243a34
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-02-01 22:17:51 +0530

    Create NthNodeEnd.py

---
+++ LinkedList/NthNodeEnd.py
@@ -0,0 +1,16 @@
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        l = 0 
+        temp = head
+        while temp is not None:
+            l+=1
+            temp = temp.next
+        t = l-n
+        if t == 0:
+            return head.next
+        point = head
+        while t != 1:
+            point = point.next
+            t-=1
+        point.next = point.next.next
+        return head
