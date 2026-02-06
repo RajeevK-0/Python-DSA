dab46789b7b07bc10d8f808dc43f34ef827975e1
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-02-06 13:50:41 +0530

    Update BSTDeletion.py

--- Binary Tree/BSTDeletion.py
+++ Binary Tree/BSTDeletion.py
@@ -1,17 +1,29 @@
-def findMin(tree):
-    return MinNode(tree.root)
-def MinNode(root):
-    if root is not None:
-        MinNode(root.left)
-        if root.left == None:
-            return root.item
-def findMax(tree):
-    return MaxNode(tree.root)
-def MaxNode(root):
-    if root is not None:
-        MaxNode(root.right)
-        if root.right == None:
-            return root.item
+# def findMin(tree):
+#     return MinNode(tree.root)
+# def MinNode(root):
+#     if root is not None:
+#         MinNode(root.left)
+#         if root.left == None:
+#             return root.item
+def findMin(tree,temp):
+    curr = temp
+    while curr.left is not None:
+        curr = curr.left
+    return curr.item
+
+# def findMax(tree):
+#     return MaxNode(tree.root)
+# def MaxNode(root):
+#     if root is not None:
+#         MaxNode(root.right)
+#         if root.right == None:
+#             return root.item
+
+def findMax(tree,temp):
+    curr = temp 
+    while curr.right is not None:
+        curr = curr.right
+    return curr.item
 def deleteNode(tree,val):
     if val > findMax(tree.root) or val< findMin(tree.root):
         raise ValueError("No Node of that val in tree")
