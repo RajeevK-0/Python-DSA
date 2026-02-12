23cec4f3f74fe24527337b36933c2c68ec726f9f
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-02-12 22:31:19 +0530

    Create kthsmallest.py

---
+++ Binary Tree/kthsmallest.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    # def inOrd(self,root,k,res):
+    #     if root is None:
+    #         return root
+    #     self.inOrd(root.left,k,res)
+    #     res.append(root.val)
+    #     self.inOrd(root.right,k,res)
+    class nodeinfo:
+        def __init__(self,count = 0 , kelement = 0):
+            self.count = count
+            self.kelement = kelement
+    def trav(self,root):
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        res = []
+
+        # res =[]
+        # self.inOrd(root,k,res)
+        # return res[k-1]
