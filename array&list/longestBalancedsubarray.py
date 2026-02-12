c32787cbcc41a93e91c98d0ba31e7e9f1c4c6f40
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-02-12 22:37:19 +0530

    Update longestBalancedsubarray.py

--- array&list/longestBalancedsubarray.py
+++ array&list/longestBalancedsubarray.py
@@ -4,18 +4,15 @@ class Solution:
         max_len = 0
         
         for i in range(n):
-            distinct_evens = set()
-            distinct_odds = set()
-            
+            evens = set()
+            odds = set()
             for j in range(i, n):
                 val = nums[j]
                 if val % 2 == 0:
-                    distinct_evens.add(val)
+                    evens.add(val)
                 else:
-                    distinct_odds.add(val)
-                
-                # Check if the current subarray [i...j] is balanced
-                if len(distinct_evens) == len(distinct_odds):
+                    odds.add(val)
+                if len(evens) == len(odds):
                     max_len = max(max_len, j - i + 1)
                     
         return max_len
