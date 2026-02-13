4a6ee8982d87dea35137c875cb34e60b412cdb43
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-02-13 22:37:31 +0530

    Update kthsmallest.py

--- Binary Tree/kthsmallest.py
+++ Binary Tree/kthsmallest.py
@@ -12,24 +12,39 @@ class Solution:
     #     self.inOrd(root.left,k,res)
     #     res.append(root.val)
     #     self.inOrd(root.right,k,res)
-    class nodeinfo:
-        def __init__(self,count = 0 , kelement = None):
-            self.count = count
-            self.kelement = kelement
-    def trav(self,root,k):
-        if root is None:
-            return self.nodeinfo()
-        left = self.trav(root.left,k)
-        if left.kelement is not None:
-            return left
-        if left.count+1 == k:
-            return self.nodeinfo(left.count+1,root.val)
-        right = self.trav(root.right,k-(left.count+1))
-        if right.kelement is not None:
-            return right
-        return self.nodeinfo(left.count+1+right.count,None)
+    # class nodeinfo:
+    #     def __init__(self,count = 0 , kelement = None):
+    #         self.count = count
+    #         self.kelement = kelement
+    # def trav(self,root,k):
+    #     if root is None:
+    #         return self.nodeinfo()
+    #     left = self.trav(root.left,k)
+    #     if left.kelement is not None:
+    #         return left
+    #     if left.count+1 == k:
+    #         return self.nodeinfo(left.count+1,root.val)
+    #     right = self.trav(root.right,k-(left.count+1))
+    #     if right.kelement is not None:
+    #         return right
+    #     return self.nodeinfo(left.count+1+right.count,None)
+    c = 0
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        return self.trav(root,k).kelement
+        self.count = k
+        self.result  = None
+        def trav(node):
+            if node is None or self.result is not None:
+                return
+            trav(node.left)
+            self.count -= 1
+            if self.count == 0:
+                self.result = node.val
+                return
+            trav(node.right)
+        trav(root)
+        return self.result
+
+        # return self.trav(root,k).kelement
         # res =[]
         # self.inOrd(root,k,res)
         # return res[k-1] 
