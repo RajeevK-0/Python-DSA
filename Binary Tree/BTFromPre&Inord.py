ce14cf9d8e0a80ba5cd78827ad6d46075e66a846
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-02-14 21:57:12 +0530

    Update BTFromPre&Inord.py

--- Binary Tree/BTFromPre&Inord.py
+++ Binary Tree/BTFromPre&Inord.py
@@ -12,10 +12,10 @@ class Solution:
             return None
         start = preorder[ps]
         mid = d[start]
-        root = TreeNode(val = start)
+        
         left = self.trav(preorder,ps+1,ps+mid-Is,inorder,Is,mid-1,d)
         right = self.trav(preorder,ps+mid-Is+1,pe,inorder,mid+1,Ie,d)
-        root = TreeNode(left = left,right = right)
+        root = TreeNode(left = left,right = right,val = start)
         return root
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         if not preorder and not inorder:
