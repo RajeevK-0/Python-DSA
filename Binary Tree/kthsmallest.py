85c46ae08115ffdc043a573b50a3eb12b3438ef6
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-02-14 21:52:24 +0530

    Update kthsmallest.py

--- Binary Tree/kthsmallest.py
+++ Binary Tree/kthsmallest.py
@@ -4,30 +4,8 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
-    # def inOrd(self,root,k,res):
-    #     if root is None:
-    #         return root
-    #     self.inOrd(root.left,k,res)
-    #     res.append(root.val)
-    #     self.inOrd(root.right,k,res)
-    # class nodeinfo:
-    #     def __init__(self,count = 0 , kelement = None):
-    #         self.count = count
-    #         self.kelement = kelement
-    # def trav(self,root,k):
-    #     if root is None:
-    #         return self.nodeinfo()
-    #     left = self.trav(root.left,k)
-    #     if left.kelement is not None:
-    #         return left
-    #     if left.count+1 == k:
-    #         return self.nodeinfo(left.count+1,root.val)
-    #     right = self.trav(root.right,k-(left.count+1))
-    #     if right.kelement is not None:
-    #         return right
-    #     return self.nodeinfo(left.count+1+right.count,None)
+    
     c = 0
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         self.count = k
