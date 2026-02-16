831f80572d7f9e66ab8167ea24424664f01e2eb1
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-02-16 22:24:21 +0530

    Update reverseBit.py

--- bit Manipulation/reverseBit.py
+++ bit Manipulation/reverseBit.py
@@ -2,10 +2,7 @@ class Solution:
     def reverseBits(self, n: int) -> int:
         res = 0
         for _ in range(32):
-            # Shift result left to make space
             res = res << 1
-            # Get the last bit of n and add it to res
             res = res | (n & 1)
-            # Shift n right to move to the next bit
             n = n >> 1
         return res
