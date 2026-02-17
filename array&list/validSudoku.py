6640cb4195bac6a9e18ff9cf0cb5b8d90b01b08f
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-02-17 22:53:08 +0530

    Update validSudoku.py

--- array&list/validSudoku.py
+++ array&list/validSudoku.py
@@ -3,24 +3,17 @@ class Solution:
         rows = [set() for _ in range(9)]
         cols = [set() for _ in range(9)]
         boxes = [set() for _ in range(9)]
-
         for i in range(9):
             for j in range(9):
                 num = board[i][j]
                 if num == '.':
                     continue
-                
-                # Check row
                 if num in rows[i]:
                     return False
                 rows[i].add(num)
-                
-                # Check column
                 if num in cols[j]:
                     return False
                 cols[j].add(num)
-                
-                # Check box
                 box_index = (i // 3) * 3 + (j // 3)
                 if num in boxes[box_index]:
                     return False
