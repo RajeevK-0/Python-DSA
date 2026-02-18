44e84ebac14cecb8d61b229547d8bc224f8f5100
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-02-18 18:06:32 +0530

    Update serializeAndDeserialize.py

--- Binary Tree/serializeAndDeserialize.py
+++ Binary Tree/serializeAndDeserialize.py
@@ -74,4 +74,45 @@ class Codec:
         for i in range(len(inord)):
             d[int(inord[i])] = i
         return self.trav(pre,0,len(pre)-1,inord,0,len(inord)-1,d)
+    
+## if duplicate is there use preorder+none method
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Codec:
+    # Encodes a tree to a single string.
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        res = []
+        def trav(root):
+            if root is None:
+                res.append('N')
+                return res
+            res.append(str(root.val))
+            trav(root.left)
+            trav(root.right)
+        trav(root)
+        return ','.join(res)
+    # Decodes your encoded data to tree.
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        if not data:
+            return None
+        pre = data.split(',')
+        i = 0
+        def trav():
+            nonlocal i 
+            if pre[i] == 'N':
+                i += 1
+                return None
+            node = TreeNode(int(pre[i]))
+            i+= 1
+            node.left = trav()
+            node.right = trav()
+            return node
+        return trav()
         
