6f16ebc8f8696f1cc1245f0bdde0dd9b96ba3397
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-02-19 12:54:24 +0530

    Update serializeAndDeserialize.py

--- Binary Tree/serializeAndDeserialize.py
+++ Binary Tree/serializeAndDeserialize.py
@@ -115,4 +115,71 @@ class Codec:
             node.right = trav()
             return node
         return trav()
+
+## DFS method visiting every node if its None then "N" 
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Codec:
+    # Encodes a tree to a single string.
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        if root is None:
+            return "N"
+        res = []
+        dq = deque()
+        dq.append(root)
+        while dq:
+            temp = dq.popleft()
+            if temp:
+                res.append(str(temp.val))
+                dq.append(temp.left)
+                dq.append(temp.right)
+            else:
+                res.append('N')
+        # def trav(root):
+        #     if root is None:
+        #         res.append('N')
+        #         return res
+        #     res.append(str(root.val))
+        #     trav(root.left)
+        #     trav(root.right)
+        # trav(root)
+        return ','.join(res)
+    # Decodes your encoded data to tree.
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        lvlord = data.split(',')
+        if lvlord[0] == 'N':
+            return None
+        root = TreeNode(val = int(lvlord[0]))
+        dq = deque([root])
         
+        i = 1
+        while dq:
+            temp = dq.popleft()
+            if lvlord[i] != 'N':
+                temp.left = TreeNode(val = int(lvlord[i]))
+                dq.append(temp.left)
+            i+=1
+            if lvlord[i] != 'N':
+                temp.right = TreeNode(val = int(lvlord[i]))
+                dq.append(temp.right)
+            i+=1
+        return root 
+        # pre = data.split(',')
+        # i = 0
+        # def trav():
+        #     nonlocal i 
+        #     if pre[i] == 'N':
+        #         i += 1
+        #         return None
+        #     node = TreeNode(int(pre[i]))
+        #     i+= 1
+        #     node.left = trav()
+        #     node.right = trav()
+        #     return node
+        # return trav()
