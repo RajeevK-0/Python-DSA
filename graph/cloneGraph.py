a891d8cd526f02e18d968c063f0f5f19f9c42d6a
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-02-22 16:36:18 +0530

    Create cloneGraph.py

---
+++ graph/cloneGraph.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def dfs(self,node,d):
+        
+        if node in d:
+            return node
+        NewNode = Node()
+        NewNode.val = node.val
+        d[node] = NewNode
+        for i in range(len(node.neighbors)):
+            NewNode.neighbors.append(self.dfs(node.neighbors[i],d))
+        return NewNode
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if node is None:
+            return None
+        d = {}
+        return self.dfs(node,d)
