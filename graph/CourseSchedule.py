6769863acee08bea4fcf93912f3aa8dd9d9091d3
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-02-23 22:14:10 +0530

    Update CourseSchedule.py

--- graph/CourseSchedule.py
+++ graph/CourseSchedule.py
@@ -11,7 +11,6 @@ class Solution:
         visit.remove(cs)
         d[cs] = []
         return True
-
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         d = {i:[] for i in range(numCourses)}
         for i,j in prerequisites:
@@ -21,3 +20,35 @@ class Solution:
             if not self.dfs(d,i,visit):
                 return False
         return True        
+#UNION FIND
+class DSU:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [1] * n
+
+    def find(self, node):
+        cur = node
+        while cur != self.parent[cur]:
+            self.parent[cur] = self.parent[self.parent[cur]]
+            cur = self.parent[cur]
+        return cur
+
+    def union(self, u, v):
+        pu = self.find(u)
+        pv = self.find(v)
+        if pu == pv:
+            return False
+        if self.rank[pv] > self.rank[pu]:
+            pu, pv = pv, pu
+        self.parent[pv] = pu
+        self.rank[pu] += self.rank[pv]
+        return True
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        dsu = DSU(n)
+        res = n
+        for u, v in edges:
+            if dsu.union(u, v):
+                res -= 1
+        return res
