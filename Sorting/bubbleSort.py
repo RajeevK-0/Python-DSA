8164e2f59a0103d9e887bd39e637e3f5c35012c5
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-02-26 16:12:17 +0530

    Update bubbleSort.py

--- Sorting/bubbleSort.py
+++ Sorting/bubbleSort.py
@@ -11,3 +11,21 @@ def bubbleeSort(a):
     return a
 j = [10,30,20,9,25]
 print(bubbleeSort(j))
+
+def modifiedBubbleSort(a):
+    n = len(a)
+    for k in range(n):
+        done = False
+        for i in range(n-k):
+            if i < n-1:
+                j = i+1
+                if a[i] > a[j]:
+                    t = a[i]
+                    a[i] = a[j]
+                    a[j] = t
+                    done = True
+        if done == False:
+            return a
+    return a
+j = [10,30,20,9,25]
+print(modifiedBubbleSort(j))
