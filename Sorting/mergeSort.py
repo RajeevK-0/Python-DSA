f93be34013781175132c26e6ad9cdf42b4d7a6e4
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-02-28 00:18:22 +0530

    Update mergeSort.py

--- Sorting/mergeSort.py
+++ Sorting/mergeSort.py
@@ -15,3 +15,31 @@ def mergeSort(a):
     return merge(l,r)
 j = [60,50,20,10,100,90,30]
 print(mergeSort(j))
+
+
+def mSort(a):
+    if len(a)>1:
+        mid = len(a)//2
+        left = mSort(a[:mid])
+        right = mSort(a[mid:])
+        i=j=k=0
+        while i<len(left) and j<len(right):
+            if left[i] < right[j]:
+                a[k] = left[i]
+                i+=1
+                k+=1
+            else:
+                a[k] = right[j]
+                j+=1
+                k+=1
+        while i< len(left):
+            a[k] = left[i]
+            i+=1
+            k+=1
+        while j< len(right):
+            a[k] = right[j]
+            j+=1
+            k+=1
+    return a
+j = [60,50,20,10,100,90,30]
+print(mSort(j))
