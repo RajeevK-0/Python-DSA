425fd75e2b212f3989ef05ab9eaa5753fbff71f9
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-03-03 20:28:42 +0530

    Update kThBinaryBitNthBinary.py

--- bit Manipulation/kThBinaryBitNthBinary.py
+++ bit Manipulation/kThBinaryBitNthBinary.py
@@ -2,10 +2,8 @@ class Solution:
     def findKthBit(self, n: int, k: int) -> str:
         if n == 1:
             return "0"
-
         length = (1 << n) - 1
         mid = (length + 1) // 2
-        
         if k == mid:
             return "1"
         elif k < mid:
