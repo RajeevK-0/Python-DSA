872c0419d5e27711a563f7a6fa3c4a8a2a2a5de2
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-03-10 22:03:54 +0530

    Update MinInRotatedSortArr.py

--- searching/MinInRotatedSortArr.py
+++ searching/MinInRotatedSortArr.py
@@ -2,11 +2,7 @@ class Solution:
     def binary(self,nums,s,e):
         mid = (s+e)//2
         if s==e:
-            return mid 
-        # if mid != e and nums[mid] > nums[mid+1] and nums[mid] > nums[mid-1]:
-        #     return mid+1
-        # if mid == e and nums[mid]>nums[mid-1] and nums[mid] > nums[s]:
-        #     return 0
+            return mid
         if nums[mid] > nums[e]:
             return self.binary(nums,mid+1,e)
         else:
