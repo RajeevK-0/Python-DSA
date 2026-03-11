2babbe7499cd6c028facd45a531030ba8b0cb693
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-03-11 22:57:43 +0530

    Update reorderLL.py

--- LinkedList/reorderLL.py
+++ LinkedList/reorderLL.py
@@ -5,19 +5,6 @@
 #         self.next = next
 
 class Solution:
-    # def reorder(self,head,res):
-    #     if head == None:
-    #         return res
-    #     res.val = head.val
-    #     t = head
-    #     head = head.next
-    #     while head.next is not None:
-    #         t = t.next
-    #         head = head.next
-    #     res.next = t
-    #     head = None
-    #     self.reorder(head,res) 
-        # return res
     def reorderList(self, head: Optional[ListNode]) -> None:
         s,f = head , head.next
         while f and f.next:
@@ -38,27 +25,3 @@ class Solution:
             first.next = second
             second.next = tmp1
             first, second = tmp1, tmp2
-        # l , r = ListNode() , ListNode()
-        # t = head
-        # lh = t
-        # while t != s:
-        #     l = t
-        #     t = t.next
-        #     l = l.next
-        # while t and t.next:
-        #     if r is None:
-        #         r = t
-        #     r.next = r
-        #     r = t
-        #     t = t.next
-        # k = lh
-        # print(l.val)
-        # print(r.val)
-        # while k is not None:
-        #     head = k
-        #     if r:
-        #         head.next = r
-        #         r = r.next
-        #     k = k.next
-        # res  = ListNode()
-        # return self.reorder(head,res)
