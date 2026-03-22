4687ed8164671d239c689569d4f0e09c43cbe2bb
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-03-22 23:40:54 +0530

    Update mergeKSorted.py

--- LinkedList/mergeKSorted.py
+++ LinkedList/mergeKSorted.py
@@ -3,25 +3,25 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def insert(self,node,res):
-        n = ListNode(node.val)
-        dummy = ListNode(0)
-        dummy.next = res
-        prev = dummy
-        while prev.next and prev.next.val < node.val:
-            prev = prev.next
-        n.next = prev.next
-        prev.next = n
-        return dummy.next      
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        res = None
-        for i in lists:
-            t = i
-            while t: 
-                res = self.insert(t,res)
-                t = t.next
-        return res
+# class Solution:
+#     def insert(self,node,res):
+#         n = ListNode(node.val)
+#         dummy = ListNode(0)
+#         dummy.next = res
+#         prev = dummy
+#         while prev.next and prev.next.val < node.val:
+#             prev = prev.next
+#         n.next = prev.next
+#         prev.next = n
+#         return dummy.next      
+#     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+#         res = None
+#         for i in lists:
+#             t = i
+#             while t: 
+#                 res = self.insert(t,res)
+#                 t = t.next
+#         return res
 
 ############################################################################
 # Definition for singly-linked list.
