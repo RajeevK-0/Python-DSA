a610dbe828e46e07a0e5c3569d82bbc8d7697937
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-03-27 23:01:38 +0530

    Update MatrixSimilarityAfterCyclicShifts.py

--- mischQ/MatrixSimilarityAfterCyclicShifts.py
+++ mischQ/MatrixSimilarityAfterCyclicShifts.py
@@ -34,4 +34,15 @@ class Solution:
             for i in range(n):
                 if l[i] != l[(i+k)%n]:
                     return False
+        return 
+########################################################################
+class Solution:
+    def areSimilar(self, mat: List[List[int]], k: int) -> bool:
+        m, n = len(mat), len(mat[0])
+        k %= n
+
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] != mat[i][(j + k) % n]:
+                    return False
         return True
