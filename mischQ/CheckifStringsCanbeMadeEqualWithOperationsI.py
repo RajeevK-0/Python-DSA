589b1e5ab6ceb4235b5ea9a7976cb5a8d661883f
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-03-29 23:17:04 +0530

    Update CheckifStringsCanbeMadeEqualWithOperationsI.py

--- mischQ/CheckifStringsCanbeMadeEqualWithOperationsI.py
+++ mischQ/CheckifStringsCanbeMadeEqualWithOperationsI.py
@@ -24,3 +24,21 @@ class Solution:
         odd_match = sorted([s1[1], s1[3]]) == sorted([s2[1], s2[3]])
         
         return even_match and odd_match
+#######################################################################
+class Solution:
+    def canBeEqual(self, s1: str, s2: str) -> bool:
+        s1_list = list(s1)
+        s2_list = list(s2)
+
+        i = 0
+       
+        while i < 2:
+            j = i + 2
+            if s1[i] != s2[i] and s1[j] != s2[j]:
+                s1_list[i], s1_list[j] = s1_list[j], s1_list[i] 
+            print("s1_list:", s1_list)
+            if s1_list == s2_list:
+                return True
+            i += 1
+            
+        return False
