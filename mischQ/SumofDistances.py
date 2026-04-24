e82fe94dd9f9dd6136f694b3eeded31615dbdb45
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-04-24 17:39:22 +0530

    Update SumofDistances.py

--- mischQ/SumofDistances.py
+++ mischQ/SumofDistances.py
@@ -2,19 +2,12 @@ class Solution:
     def distance(self, nums: List[int]) -> List[int]:
         n = len(nums)
         res = [0] * n
-        
-        # Maps number -> [count_seen, total_sum_of_indices]
-        # We'll use two passes (Left-to-Right and Right-to-Left)
-        
-        # Pass 1: Calculate distances to the LEFT
         count = defaultdict(int)
         sum_indices = defaultdict(int)
         for i, x in enumerate(nums):
             res[i] += (count[x] * i) - sum_indices[x]
             count[x] += 1
             sum_indices[x] += i
-            
-        # Pass 2: Calculate distances to the RIGHT
         count.clear()
         sum_indices.clear()
         for i in range(n - 1, -1, -1):
@@ -22,5 +15,4 @@ class Solution:
             res[i] += sum_indices[x] - (count[x] * i)
             count[x] += 1
             sum_indices[x] += i
-            
         return res
