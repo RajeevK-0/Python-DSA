55121380a1939b1aada010c10ef6cbf3c006fba1
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-05-10 22:47:01 +0530

    Update MaximumNumberofJumpstoReachtheLastIndex.py

--- array&list/MaximumNumberofJumpstoReachtheLastIndex.py
+++ array&list/MaximumNumberofJumpstoReachtheLastIndex.py
@@ -1,13 +1,10 @@
 class Solution:
     def maximumJumps(self, nums: List[int], target: int) -> int:
         n = len(nums)
-        # Initialize DP array with -1
         dp = [-1] * n
-        dp[0] = 0 # Starting point
-        
+        dp[0] = 0
         for j in range(1, n):
             for i in range(j):
-                # Check if previous index i is reachable and jump is valid
                 if dp[i] != -1 and abs(nums[j] - nums[i]) <= target:
                     dp[j] = max(dp[j], dp[i] + 1)
                     
