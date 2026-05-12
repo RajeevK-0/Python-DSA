8f78aa8af611d1fd01c3800d748f5bb782d62c1a
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-05-12 11:23:19 +0530

    Create minEnergyTofinishTask.py

---
+++ array&list/minEnergyTofinishTask.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minimumEffort(self, tasks: List[List[int]]) -> int:
+        # Sort by the difference between minimum and actual (descending)
+        tasks.sort(key=lambda x: x[1] - x[0], reverse=True)
+        
+        initial_energy = 0
+        current_energy = 0
+        
+        for actual, minimum in tasks:
+            if current_energy < minimum:
+                # We need to increase our starting pool to meet the requirement
+                shortfall = minimum - current_energy
+                initial_energy += shortfall
+                current_energy += shortfall
+            
+            # Spend the energy
+            current_energy -= actual
+            
+        return initial_energy
+        
+        # need = 0
+        # for actual, minimum in reversed(tasks):
+        #     need = max(minimum, need + actual)
+        # return need
