c6ce0d877124f4a757a19b78320a2e20b1ae12a4
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-05-20 13:50:19 +0530

    Create prefCommonArrOf2Arr.py

---
+++ array&list/prefCommonArrOf2Arr.py
@@ -0,0 +1,33 @@
+class Solution:
+    def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
+        s = set()
+        c = []
+        com = 0
+        for i in range(len(A)):
+            if A[i] in s :
+                com+=1
+            else:
+                s.add(A[i])
+            if B[i] in s:
+                com+=1
+            else:
+                s.add(B[i])
+            # if A[i] == B[i]:
+            #     com+=1
+            c.append(com)
+        return c
+        # c = []
+        # ma = {}
+        # mb = {}
+        # com = 0
+        # for i in range(len(A)):
+        #     ma[A[i]] = i
+        #     mb[B[i]] = i
+        #     if A[i] == B[i] :
+        #         com +=1
+        #     if (A[i] in mb and A[i] != B[i] ) :
+        #         com+=1
+        #     if (B[i] in ma and A[i] != B[i]):
+        #         com+=1
+        #     c.append(com)
+        # return c
