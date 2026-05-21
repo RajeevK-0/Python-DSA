30ec3065a9ff0b33f3a01ae53cc0c4af915c4f75
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-05-21 23:32:53 +0530

    Update findLenOfLongComPref.py

--- array&list/findLenOfLongComPref.py
+++ array&list/findLenOfLongComPref.py
@@ -1,24 +1,18 @@
 class Solution:
     def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
         prefixes = set()
-        
-        # Step 1: Put all mathematical prefixes of arr1 into a set
         for val in arr1:
             while val > 0:
                 prefixes.add(val)
-                val //= 10  # Strips the last digit mathematically
-                
+                val //= 10 
         ans = 0
-        
-        # Step 2: Crop numbers in arr2 and check if they exist in our set
         for val in arr2:
             while val > 0:
                 if val in prefixes:
                     # len(str(val)) gives us the prefix length
                     ans = max(ans, len(str(val)))
-                    break # Since we go top-down, the first match is the longest for THIS number
+                    break 
                 val //= 10
-                
         return ans
 
     # def isPrefix(self,x,y):
