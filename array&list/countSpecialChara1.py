56a6d876192dcbd047f2a53b296c3fb98746aa8f
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-05-26 10:59:12 +0530

    Update countSpecialChara1.py

--- array&list/countSpecialChara1.py
+++ array&list/countSpecialChara1.py
@@ -7,3 +7,15 @@ class Solution:
                 if i.lower() in s:
                     ans+=1
         return ans
+    #m2
+    # d = {}
+        # visited = [0]*26
+        # for i in word:
+        #     d[i] = d.get(i,0)+1
+        # special = 0
+        # for i in d.keys():
+        #     if i != i.lower():
+        #         if i in d and ord(i.lower())-97==0 :
+        #             special+=1
+        #             visited[ord(i.lower())-97] = 1
+        # return special
