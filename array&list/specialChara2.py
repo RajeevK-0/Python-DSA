a384dedc1bf961c83bf3187b73483a0f5c2ebfb6
RajeevK-0 <141414587+RajeevK-0@users.noreply.github.com>
2026-05-27 22:41:54 +0530

    Create specialChara2.py

---
+++ array&list/specialChara2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def numberOfSpecialChars(self, word: str) -> int:
+        d = {}
+        for i in range(len(word)):
+            if word[i] not in d:
+                d[word[i]] = [i]
+            else:
+                d[word[i]].append(i)
+        ans =0
+        vis = [0]*26
+        for i in word:
+            if i!=i.upper() and vis[ord(i)-97]!=1:
+                if i in d:
+                    if i.upper() in d:
+                        if d[i][-1] < d[i.upper()][0]:
+                            ans+=1 
+                            vis[ord(i)-97] = 1
+        return ans
+        
